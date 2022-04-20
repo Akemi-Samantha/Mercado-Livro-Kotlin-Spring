46c4a3998ddd0b3ea0e77599bed240595fc363cb
akemi.nakayama <akemi.nakayama@VALEMOBI.CORP>
2022-04-20 14:34:32 -0300

    Tentativa numero 2

--- src/main/kotlin/com/mercadolivro/controllers/CustumerController.kt
+++ src/main/kotlin/com/mercadolivro/controllers/CustumerController.kt
@@ -1,14 +1,17 @@
-package com.controllers
+package com.mercadolivro.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
+@RequestMapping("customer")
 class CustumerController {
 
     @GetMapping
-    fun helloWorld(): String{
-        return "Hellooooooo"
+    fun getCustomer(): String{
+        return "Custuomer number one"
     }
 
 }
